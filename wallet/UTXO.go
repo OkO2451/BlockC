eee589bd08ea82fdba88dbd3f0b92b49c7d8d22f
OkO2451 <bobot.imrane@student.emi.ac.ma>
2024-02-26 02:04:43 +0100

    Made the CLI into http requests with gin

---
+++ wallet/UTXO.go
@@ -0,0 +1,35 @@
+package transactions
+
+import (
+	"encoding/hex"
+	"github.com/OkO2451/BlockC/blockchain"
+)
+
+type UTXOset struct {
+	Blockchain *blockchain.bChain
+}
+
+// FindSpendableOutputs finds and returns unspent outputs to reference in inputs
+func (u UTXOset) FindSpendableOutputs(pubKeyHash []byte, amount int) (int, map[string][]int) {
+	unspentOutputs := make(map[string][]int)
+	unspentTXs := u.Blockchain.FindUnspentTransactions(pubKeyHash)
+	accumulated := 0
+
+Work:
+	for _, tx := range unspentTXs {
+		txID := hex.EncodeToString(tx.ID)
+
+		for outIdx, out := range tx.Vout {
+			if out.CanBeUnlockedWith(pubKeyHash) && accumulated < amount {
+				accumulated += out.Value
+				unspentOutputs[txID] = append(unspentOutputs[txID], outIdx)
+
+				if accumulated >= amount {
+					break Work
+				}
+			}
+		}
+	}
+
+	return accumulated, unspentOutputs
+}
